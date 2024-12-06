e3566e2e27876889b50c12615631cca33abf752e
Clmaldonado <clmaldonado2019@udec.cl>
2024-12-06 03:20:56 -0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -19,6 +19,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Ruta para servir el archivo index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Simulación de usuarios registrados
 const users = [
     {
